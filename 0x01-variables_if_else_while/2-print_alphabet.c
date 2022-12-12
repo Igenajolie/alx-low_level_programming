47b353b62d6cdbc55da0230bc3701e479a0d7674
root <igenajolie001@gmail.com>
2022-12-12 14:31:21 -0800

    alphabatic

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return:	Always	0
+ */
+int	main(void)
+{	char	chr;
+
+	for	(chr = 'a'; chr <= 'z' ; chr++)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return	(0);
+
+}
