bef74af85530042ef2d842bb64a737ac8f63c3c4
root <igenajolie001@gmail.com>
2022-12-12 17:29:58 -0800

    combing

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main	-	entry
+ *
+ * Return:	Always	0
+ */
+int	main(void)
+{
+	int	i;
+
+	for	(i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar('\t');
+	}
+	putchar('\n');
+	return	(0);
+}
