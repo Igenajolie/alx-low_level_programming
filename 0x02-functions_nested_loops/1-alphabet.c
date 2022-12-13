cf2e4bdf1948ec07d788387919c0745969005fe9
root <igenajolie001@gmail.com>
2022-12-13 12:33:01 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet	-	prints the alpha in lowercase
+ */
+void	print_alphabet(void)
+{
+	char	s;
+
+	for	(s = 'a' ; s <= 'z' ; s++)
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
